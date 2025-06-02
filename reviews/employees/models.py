fb27115fedf8bf5ef6203bb55a9b67648dd6fa0b
Inert Tila <137422939+inerttila@users.noreply.github.com>
2025-06-02 12:28:15 +0200

    Update models.py

--- reviews/employees/models.py
+++ reviews/employees/models.py
@@ -6,33 +6,21 @@ class Employee(models.Model):
     dep = models.TextField(max_length=30, null=True)
     birthdate = models.CharField(max_length=30, null=True)
     salary = models.TextField(max_length=30, null=True)
-    created_at = models.DateTimeField(auto_now_add=True, null=True)
-    
-    class Meta:
-        ordering = ['created_at']
+
 
     def __str__(self):
         return self.fullname
 
-
 class Review(models.Model):
-    SHOP_CHOICES = [
-        ('skaitech', 'skaitech'),
-        ('3dskai', '3dskai'),
+    COMPANY_CHOICES = [
+        ('company 1', 'company 1'),
+        ('company 2', 'company 2'),
     ]
 
-    userName = models.CharField(max_length=100, null=True)
-    thumbsUppAt = models.DateTimeField(null=True, blank=True)  # Store timestamp instead of count
-    thumbsDownAt = models.DateTimeField(null=True, blank=True)  # Store timestamp instead of count
-    shop = models.CharField(
-        max_length=20, 
-        choices=SHOP_CHOICES,
-        null=True,
-    )
-    created_at = models.DateTimeField(auto_now_add=True, null=True)
-
-    class Meta:
-        ordering = ['created_at']
+    userName = models.CharField(max_length=100, null=True, unique=True)
+    timein = models.DateTimeField(null=True, blank=True)
+    timeout = models.DateTimeField(null=True, blank=True)
+    company = models.CharField(max_length=20, choices=COMPANY_CHOICES, null=True)
 
     def __str__(self):
         return self.userName
